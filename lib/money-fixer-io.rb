b062e6a9cf12c0ac8f923b4abd24070f72303ca6
kaspernj <kaspernj@gmail.com>
2016-09-30 17:38:52 +0200

    First commit

---
+++ lib/money-fixer-io.rb
@@ -0,0 +1,148 @@
+require "money"
+require "money/rates_store/rate_removal_support"
+require "open-uri"
+
+class Money::Bank::FixerIo < Money::Bank::VariableExchange
+  SERVICE_HOST = "api.fixer.io".freeze
+  SERVICE_PATH = "/".freeze
+
+  # @return [Hash] Stores the currently known rates.
+  attr_reader :rates
+
+  class << self
+    # @return [Integer] Returns the Time To Live (TTL) in seconds.
+    attr_reader :ttl_in_seconds
+
+    # @return [Time] Returns the time when the rates expire.
+    attr_reader :rates_expiration
+
+    ##
+    # Set the Time To Live (TTL) in seconds.
+    #
+    # @param [Integer] the seconds between an expiration and another.
+    def ttl_in_seconds=(value)
+      @ttl_in_seconds = value
+      refresh_rates_expiration! if ttl_in_seconds
+    end
+
+    ##
+    # Set the rates expiration TTL seconds from the current time.
+    #
+    # @return [Time] The next expiration.
+    def refresh_rates_expiration!
+      @rates_expiration = Time.now + ttl_in_seconds
+    end
+  end
+
+  def initialize(*)
+    super
+    @store.extend Money::RatesStore::RateRemovalSupport
+  end
+
+  ##
+  # Clears all rates stored in @rates
+  #
+  # @return [Hash] The empty @rates Hash.
+  #
+  # @example
+  #   @bank = GoogleCurrency.new  #=> <Money::Bank::GoogleCurrency...>
+  #   @bank.get_rate(:USD, :EUR)  #=> 0.776337241
+  #   @bank.flush_rates           #=> {}
+  def flush_rates
+    store.clear_rates
+  end
+
+  ##
+  # Clears the specified rate stored in @rates.
+  #
+  # @param [String, Symbol, Currency] from Currency to convert from (used
+  #   for key into @rates).
+  # @param [String, Symbol, Currency] to Currency to convert to (used for
+  #   key into @rates).
+  #
+  # @return [Float] The flushed rate.
+  #
+  # @example
+  #   @bank = GoogleCurrency.new    #=> <Money::Bank::GoogleCurrency...>
+  #   @bank.get_rate(:USD, :EUR)    #=> 0.776337241
+  #   @bank.flush_rate(:USD, :EUR)  #=> 0.776337241
+  def flush_rate(from, to)
+    store.remove_rate(from, to)
+  end
+
+  ##
+  # Returns the requested rate.
+  #
+  # It also flushes all the rates when and if they are expired.
+  #
+  # @param [String, Symbol, Currency] from Currency to convert from
+  # @param [String, Symbol, Currency] to Currency to convert to
+  #
+  # @return [Float] The requested rate.
+  #
+  # @example
+  #   @bank = GoogleCurrency.new  #=> <Money::Bank::GoogleCurrency...>
+  #   @bank.get_rate(:USD, :EUR)  #=> 0.776337241
+  def get_rate(from, to, args = {})
+    expire_rates
+
+    if args[:exchanged_at]
+      fetch_rate(from, to, exchanged_at: args.fetch(:exchanged_at))
+      raise "stub"
+    else
+      store.get_rate(from, to) || store.add_rate(from, to, fetch_rate(from, to))
+    end
+  end
+
+  ##
+  # Flushes all the rates if they are expired.
+  #
+  # @return [Boolean]
+  def expire_rates
+    if self.class.ttl_in_seconds && self.class.rates_expiration <= Time.now
+      flush_rates
+      self.class.refresh_rates_expiration!
+      true
+    else
+      false
+    end
+  end
+
+private
+
+  ##
+  # Queries for the requested rate and returns it.
+  #
+  # @param [String, Symbol, Currency] from Currency to convert from
+  # @param [String, Symbol, Currency] to Currency to convert to
+  #
+  # @return [BigDecimal] The requested rate.
+  def fetch_rate(from, to, args = {})
+    from = Currency.wrap(from)
+    to = Currency.wrap(to)
+
+    data = JSON.parse(build_uri(from, to, args).read)
+    rate = data.fetch("rates").fetch(to)
+    rate = 1 / extract_rate(build_uri(to, from).read) if rate < 0.1
+    rate
+  end
+
+  ##
+  # Build a URI for the given arguments.
+  #
+  # @param [Currency] from The currency to convert from.
+  # @param [Currency] to The currency to convert to.
+  #
+  # @return [URI::HTTP]
+  def build_uri(from, _to, _args = {})
+    query = "base=#{from.iso_code}"
+    query << "&date=#{args.fetch(:exchange_at).strftime("%Y-%m-%D")}"
+
+    uri = URI::HTTP.build(
+      host: SERVICE_HOST,
+      path: SERVICE_PATH,
+      query: query
+    )
+    uri
+  end
+end
--- spec/money-fixer-io_spec.rb
+++ spec/money-fixer-io_spec.rb
@@ -1,7 +1,7 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
 
 describe "MoneyFixerIo" do
   it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+    raise "hey buddy, you should probably rename this file and start specing for real"
   end
 end
